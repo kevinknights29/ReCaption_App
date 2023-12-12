89a733a852fda1e2846d38f97649d71ad0d8145f
Kevin Knights <kevinknights29@hotmail.com>
2023-12-11 23:14:28 -0500

    WIP: getting meta page-id

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from __future__ import annotations
 
+import os
+
+from dotenv import find_dotenv
+from dotenv import load_dotenv
+from facebook_business.api import FacebookAdsApi
+
+
+_ = load_dotenv(find_dotenv())
+META_APP_ID = os.getenv("META_APP_ID")
+META_APP_SECRET = os.getenv("META_APP_SECRET")
+META_ACCESS_TOKEN = os.getenv("META_ACCESS_TOKEN")
+
 
 def main():
-    pass
+    FacebookAdsApi.init(META_APP_ID, META_APP_SECRET, META_ACCESS_TOKEN)
 
 
 if __name__ == "__main__":
